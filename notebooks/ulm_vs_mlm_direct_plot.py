43f02be5c87af65345d40699cde116dc8b5e5cc5
AlexanderFastner <alexanderfastner@gmail.com>
2024-05-09 17:10:02 +0200

    gzip and ulm png

--- notebooks/ulm_vs_mlm_direct_plot.py
+++ notebooks/ulm_vs_mlm_direct_plot.py
@@ -2,12 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("../data/ULM_vs_MLM_direct.csv", index_col=0)
+data = pd.read_csv("../data/ulm_vs_mlm_direct.csv", index_col=0)
 print(data)
-# print()
-# print(data.loc["Top_25"])
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(14, 8), sharex=True)
 
 ## Pearson and Spearman Correlations
 ax1.plot(data.loc["Pearson"], color='blue', label='Pearson')
@@ -21,7 +19,7 @@ ax1.legend(loc='upper left')
 ax2.plot(data.loc["Top_25"], color='purple', label='Top 25%')
 ax2.plot(data.loc["Top_10%"], color='brown', label='Top 10%')
 ax2.set_xlabel('Columns')
-ax2.set_xticks(np.arange(6))
+ax2.set_xticks(np.arange(8))
 ax2.set_ylabel('Percentage overlap')
 ax2.tick_params('y', colors='red')
 ax2.set_ylim(0, 100)
@@ -29,4 +27,4 @@ ax2.set_xticklabels(['WT_T01', 'WT_T02', 'MUT_T01', 'MUT_T02', 'Control_WT_T01',
 ax2.legend(loc='upper left')
 
 plt.show()
-plt.savefig('../figures/ulm_mlm/ULM_vs_MLM_direct.png', dpi=300)
+#plt.savefig('../figures/ulm_mlm/ULM_vs_MLM_direct.png', dpi=300)
